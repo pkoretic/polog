314b83a3923205ade9cee4fab3a9735db92f51d6
Petar Koretic <petar.koretic@gmail.com>
2017-05-07 14:33:10 +0200

    minor style fixes
    Signed-off-by: Petar Koretic <petar.koretic@gmail.com>

--- index.js
+++ index.js
@@ -10,22 +10,22 @@ module.exports = function(options = {})
 	const JEOL = '"]' + EOL
 
     return json ? format ? {
-        info: (...msg) => { writeSync(1, '[' + JSON.stringify(prefix())  + ',"I","' + format(...msg) + JEOL )},
+        info: (...msg) => { writeSync(1, '[' + JSON.stringify(prefix()) + ',"I","' + format(...msg) + JEOL )},
         debug: !debugMode ? ()=>{} : (...msg) => { writeSync(1, '[' + JSON.stringify(prefix()) + ',"D","' + format(...msg) + JEOL )},
         error: (...msg) => { writeSync(2, '[' + JSON.stringify(prefix()) + ',"E","' + format(...msg) + JEOL )},
         warn: (...msg) => { writeSync(2, '[' + JSON.stringify(prefix()) + ',"W","' + format(...msg) + JEOL )}
     } : {
-        info: msg => { writeSync(1, '[' + JSON.stringify(prefix())  + ',"I","' + msg + JEOL )},
+        info: msg => { writeSync(1, '[' + JSON.stringify(prefix()) + ',"I","' + msg + JEOL )},
         debug: !debugMode ? ()=>{} : msg => { writeSync(1, '[' + JSON.stringify(prefix()) + ',"D","' + msg + JEOL )},
         error: msg => { writeSync(2, '[' + JSON.stringify(prefix()) + ',"E","' + msg + JEOL )},
         warn: msg  => { writeSync(2, '[' + JSON.stringify(prefix()) + ',"W","' + msg + JEOL )}
     } : format ? {
-        info: (...msg) => { writeSync(1, prefix()  + " I " + format(...msg) + EOL )},
+        info: (...msg) => { writeSync(1, prefix() + " I " + format(...msg) + EOL )},
         debug: !debugMode ? ()=>{} : (...msg) => { writeSync(1, prefix() + " D " + format(...msg) + EOL )},
         error: (...msg) => { writeSync(2, prefix() + " E " + format(...msg) + EOL )},
         warn: (...msg) => { writeSync(2, prefix() + " W " + format(...msg) + EOL )}
     } : {
-        info: msg => { writeSync(1, prefix()  + " I " + msg + EOL )},
+        info: msg => { writeSync(1, prefix() + " I " + msg + EOL )},
         debug: !debugMode ? ()=>{} : msg => { writeSync(1, prefix() + " D " + msg + EOL )},
         error: msg => { writeSync(2, prefix() + " E " + msg + EOL )},
         warn: msg  => { writeSync(2, prefix() + " W " + msg + EOL )}

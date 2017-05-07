e501f711b8ea33722922cf73c4c57ae4718bb4bc
Petar Koretic <petar.koretic@gmail.com>
2017-05-07 11:35:02 +0200

    use arrow functions
    Signed-off-by: Petar Koretic <petar.koretic@gmail.com>

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ module.exports = function(options = {})
     const EOL = require('os').EOL
 
     return format ? {
-        info: function(...msg) { writeSync(1, prefix()  + " I " + format(...msg) + EOL )},
-        debug: !debugMode ? ()=>{} : function(...msg) { writeSync(1, prefix() + " D " + format(...msg) + EOL )},
-        error: function(...msg) { writeSync(2, prefix() + " E " + format(...msg) + EOL )},
-        warn: function(...msg) { writeSync(2, prefix() + " W " + format(...msg) + EOL )}
+        info: (...msg) => { writeSync(1, prefix()  + " I " + format(...msg) + EOL )},
+        debug: !debugMode ? ()=>{} : (...msg) => { writeSync(1, prefix() + " D " + format(...msg) + EOL )},
+        error: (...msg) => { writeSync(2, prefix() + " E " + format(...msg) + EOL )},
+        warn: (...msg) => { writeSync(2, prefix() + " W " + format(...msg) + EOL )}
     } : {
-        info: function(msg) { writeSync(1, prefix()  + " I " + msg + EOL )},
-        debug: !debugMode ? ()=>{} : function(msg) { writeSync(1, prefix() + " D " + msg + EOL )},
-        error: function(msg) { writeSync(2, prefix() + " E " + msg + EOL )},
-        warn: function(msg) { writeSync(2, prefix() + " W " + msg + EOL )}
+        info: msg => { writeSync(1, prefix()  + " I " + msg + EOL )},
+        debug: !debugMode ? ()=>{} : msg => { writeSync(1, prefix() + " D " + msg + EOL )},
+        error: msg => { writeSync(2, prefix() + " E " + msg + EOL )},
+        warn: msg  => { writeSync(2, prefix() + " W " + msg + EOL )}
     }
 }
